8f2afa9993c4dc6af54f6b2701aa9abb9d11a762
fyolnish <f@ninjakitten.us>
2010-10-20 15:15:18 +0900

    Added a licensing clause (BSD)

--- Source/PadKontrolConstants.c
+++ Source/PadKontrolConstants.c
@@ -1,8 +1,39 @@
-#import "PadKontrolConstants.h"
-
+////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  PadKontrolConstants
+//
+////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2010, Fjölnir Ásgeirsson
+// 
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification, 
+// are permitted provided that the following conditions are met:
+// 
+// Redistributions of source code must retain the above copyright notice, this list of conditions
+// and the following disclaimer.
+// Redistributions in binary form must reproduce the above copyright notice, this list of
+// conditions and the following disclaimer in the documentation and/or other materials provided with 
+// the distribution.
+// Neither the name of Fjölnir Ásgeirsson, ninja kitten nor the names of its contributors may be 
+// used to endorse or promote products derived from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+////////////////////////////////////////////////////////////////////////////////////////////
 // Format of an sysex message to/from the PadKONTROL:
 // F0* 42* 40* 6E* 08* cd nn dd F7  - Bytes marked by * are constant in all messages.
-
+//
 // F0: Exclusive status byte, 42: KORG manufacturer identifier, 40: Device ID
 // 6E: Means software project (Family ID), not sure about the significance (but it's constant so it doesn't really matter)
 // 08: PadKONTROL (SubID)
@@ -19,6 +50,8 @@
 
 // For further reference read: 
 // https://docs.google.com/viewer?url=http://www.thecovertoperators.org/uploads/PadKONTROL%2520imp.pdf
+
+#import "PadKontrolConstants.h"
 
 #pragma mark -
 #pragma mark BUILDING BLOCKS
